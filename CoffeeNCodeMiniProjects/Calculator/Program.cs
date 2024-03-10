d945be07e124ee1f156cea8b6a97c21cd7f02df5
codespace-justin <justin@codespace.co.za>
2024-03-10 17:31:46 +0200

    creates boilerplate for calculator class + logic for returning user input

---
+++ CoffeeNCodeMiniProjects/Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to my console calculator \n");
+
+Console.Write("Please enter your first number: ");
+int num1Input = Convert.ToInt32( Console.ReadLine() );
+
+Console.WriteLine();
+
+Console.Write("Please enter your second number: ");
+int num2Input = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"\n" + num1Input + " | " + num2Input);
