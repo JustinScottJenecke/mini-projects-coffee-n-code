1a5359ebd0c7567aad8f80f3683d244809fc8683
codespace-justin <justin@codespace.co.za>
2024-03-10 17:38:47 +0200

    fix: previous commit had no changes, so this commit adds the code outlined in previous commit

--- CoffeeNCodeMiniProjects/Calculator/Calculator.cs
+++ CoffeeNCodeMiniProjects/Calculator/Calculator.cs
@@ -1,13 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Calculator
 {
     class Calculator
-    {
+    { 
+        public Calculator() {
+        }
 
+        public static string[] GetUserInput() {
+
+            string[] userInput = new string[3];
+
+            Console.Write("Please enter your first number: ");
+            userInput[0] = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.Write("Please enter your second number: ");
+            userInput[1] = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.Write("Please the operation you would like to perform:");
+            userInput[2] = Console.ReadLine();
+
+            return userInput;
+        }
+
+        public static int Addition() {
+            return 0;
+        }
+        public static int Subtraction()
+        {
+            return 0;
+        }
+        public static int Mutiplication()
+        {
+            return 0;
+        }
+        public static int Division()
+        {
+            return 0;
+        }
+        public static int Modulo() {
+            return 0;
+        }
     }
 }
--- CoffeeNCodeMiniProjects/Calculator/Program.cs
+++ CoffeeNCodeMiniProjects/Calculator/Program.cs
@@ -1,12 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to my console calculator \n");
+﻿using System;
 
-Console.Write("Please enter your first number: ");
-int num1Input = Convert.ToInt32( Console.ReadLine() );
-
-Console.WriteLine();
-
-Console.Write("Please enter your second number: ");
-int num2Input = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine($"\n" + num1Input + " | " + num2Input);
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
