66c3e2df52b93da2391044fee56d56bf9597aa99
codespace-justin <justin@codespace.co.za>
2024-03-11 15:21:02 +0200

    adds logic to mathematical operation functions

--- CoffeeNCodeMiniProjects/Calculator/Calculator.cs
+++ CoffeeNCodeMiniProjects/Calculator/Calculator.cs
@@ -124,23 +124,23 @@ namespace Calculator
         }
 
         public static int Addition(int num1, int num2) {
-            return 0;
+            return num1 + num2;
         }
         public static int Subtraction(int num1, int num2)
         {
-            return 0;
+            return num1 - num2;
         }
         public static int Multiplication(int num1, int num2)
         {
-            return 0;
+            return num1 * num2;
         }
         public static int Division(int num1, int num2)
         {
-            return 0;
+            return num1 / num2;
         }
         public static int Difference(int num1, int num2) 
         {
-            return 0;
+            return num1 % num2;
         }
     }
 }
