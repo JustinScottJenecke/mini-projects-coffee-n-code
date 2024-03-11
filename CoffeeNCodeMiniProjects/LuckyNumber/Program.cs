77465bbe8b64faa56f34b0acf50da62726ac6462
codespace-justin <justin@codespace.co.za>
2024-03-11 23:18:19 +0200

    ups realNumber range from 7 to 100 + improves guess prompt text

--- CoffeeNCodeMiniProjects/LuckyNumber/Program.cs
+++ CoffeeNCodeMiniProjects/LuckyNumber/Program.cs
@@ -7,15 +7,16 @@
 
             Random numberGenerator = new Random();
 
-            int realNumber = numberGenerator.Next(1, 8); // generate number between 1 - 7
+            int realNumber = numberGenerator.Next(1, 101); // generate number between 1 - 7
             Console.WriteLine(realNumber);
 
-            Console.WriteLine("Please guess a number between 1 and 7");
+            Console.WriteLine("Please guess a number between 1 and 100");
             Console.Write("Guess: ");
             int guess = Int32.Parse(Console.ReadLine());
 
             while (guess != realNumber) {
                 Console.WriteLine("Unlucky, you guessed wrong, try again.");
+                Console.Write("Guess: ");
                 guess = Int32.Parse(Console.ReadLine());
             }
 
