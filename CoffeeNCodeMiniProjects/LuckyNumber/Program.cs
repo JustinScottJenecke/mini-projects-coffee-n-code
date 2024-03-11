f8c6550267c2bedf05deeb11093da1b45e321d71
codespace-justin <justin@codespace.co.za>
2024-03-11 23:01:27 +0200

    creates new lucky number projcet and instantiates number generator object

---
+++ CoffeeNCodeMiniProjects/LuckyNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LuckyNumber {
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the lucky number!");
+
+            Random numberGenerator = new Random();
+
+            int realNumber = numberGenerator.Next(1, 8); // generate number between 1 - 7
+            Console.WriteLine(realNumber);
+
+            Console.ReadLine();
+        }
+    }
+}
