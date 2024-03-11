c4d49e51db57d3f71ff4b4aefd01c3a65a0be803
codespace-justin <justin@codespace.co.za>
2024-03-11 23:45:05 +0200

    spacing changes

--- CoffeeNCodeMiniProjects/LuckyNumber/Program.cs
+++ CoffeeNCodeMiniProjects/LuckyNumber/Program.cs
@@ -6,6 +6,7 @@
             Console.WriteLine("Guess the lucky number!");
 
             Random numberGenerator = new Random();
+            int totalGuesses = 0;
 
             int realNumber = numberGenerator.Next(1, 101); // generate number between 1 - 7
             // Console.WriteLine(realNumber);
@@ -13,15 +14,19 @@
             Console.WriteLine("Please guess a number between 1 and 100");
             Console.Write("Guess: ");
             int guess = Int32.Parse(Console.ReadLine());
+            totalGuesses++;
 
             while (guess != realNumber) {
 
                 Console.WriteLine($"Unlucky, you guessed wrong, try something {(guess > realNumber ? "smaller" : "bigger")}");
                 Console.Write("Guess: ");
                 guess = Int32.Parse(Console.ReadLine());
+                totalGuesses++;
             }
 
+            Console.WriteLine();
             Console.WriteLine("Congrats, you are lucky! The Lucky Number was:" + realNumber);
+            Console.WriteLine("Total attempts: " + totalGuesses);
         }
     }
 }
