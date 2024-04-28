ab0bdefdbb2a38ac370d717c4135f2ca3b514536
JustinScottJenecke <justinjenecke@gmail.com>
2024-04-28 16:52:02 +0200

    creates timespan example

---
+++ CoffeeNCodeMiniProjects/derek-banas-basics/DateTimePractice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DerekBanasEasy
+{
+    public class DateTimePractice
+    {
+        // Basics of DateTime object
+        public void createDate() { 
+            DateTime testDate = DateTime.Now;
+            Console.WriteLine($"Day of the week: {testDate.DayOfWeek}");
+            // testDate.AddMonths(1);
+            Console.WriteLine($"Next month of created date {testDate.Date} is: {testDate.AddMonths(1).Date}");
+        }
+
+        // Basics of Timespan - used to define a time
+        public void setLunchTime() {
+            TimeSpan lunchTime = new TimeSpan(12, 30, 0);
+            lunchTime = lunchTime.Add(new TimeSpan(0, 30, 0)); // set lunch time to 13:00 
+            
+            Console.WriteLine(lunchTime.ToString());
+        }
+
+    }
+
+
+}
---
+++ CoffeeNCodeMiniProjects/derek-banas-basics/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DerekBanasEasy;
+class Program {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("test");
+        DateTimePractice date = new DateTimePractice();
+
+        date.createDate();
+
+        date.setLunchTime();
+    }
+}
