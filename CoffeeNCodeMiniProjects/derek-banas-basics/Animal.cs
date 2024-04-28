4822e248434f82d5895f2f617921084eb0d3f32e
JustinScottJenecke <justinjenecke@gmail.com>
2024-04-28 19:35:01 +0200

    creates animal class with default ctor and argument ctor

---
+++ CoffeeNCodeMiniProjects/derek-banas-basics/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DerekBanasEasy
+{
+    public class Animal
+    {
+        public string name;
+        public string sound;
+        static int numAnimals = 0;
+
+        public Animal()
+        {
+            this.name = "no name";
+            this.sound = "no sound";
+            numAnimals++;
+        }
+        public Animal(string name, string sound)
+        {
+            
+            this.name = name;
+            this.sound = sound;
+            numAnimals++;
+        }
+    }
+}
